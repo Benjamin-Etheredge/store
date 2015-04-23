a97c3b50688d593659c012b79f9e03c6e7f29051
Tyler Yasaka <web@tyleryasaka.me>
2015-04-22 21:34:05 -0500

    Created basic structure for our node app.

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+
+// respond with "Hello World!" on the homepage
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// accept POST request on the homepage
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+
+// accept PUT request at /user
+app.put('/user/:username', function (req, res) {
+  var username = req.params.username;
+  res.send('Hello, '+username);
+});
+
+// accept DELETE request at /user
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+});
+
+
+var server = app.listen(8027, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
+
