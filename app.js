d9b56b15e97784d646e03a8fd469a4442d48d0b5
Tyler Yasaka <tyleryasaka@users.noreply.github.com>
2015-04-25 10:09:11 -0500

    It woooorrrrrrkkkkkkksssssssssssssssssss!

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ var mongoClient = require('mongodb').MongoClient; // initializes the mongodb lib
 // callback that passes either an error or the db... just like in the mongo command client when we typed use <db>
 var mongoClient = require('mongodb').MongoClient; // initializes the mongodb library and gets a client object
 
+var collection;
 
 // callback that passes either an error or the db... just like in the mongo command client when we typed use <db>
 
 mongoClient.connect("mongodb://localhost:27017/store", function(err, db) { 
-
 
   if(!err) {
 
@@ -19,36 +19,10 @@ mongoClient.connect("mongodb://localhost:27017/store", function(err, db) {
        if you want to use multiple collections, 
        you can initialize and use other variables
        e.g. collection2 = db.collection('<collection2>'); */
-    var collection = db.collection('students');
+    collection = db.collection('store');
 
 
     console.log("We are connect to mongodb...");
-
-	// respond with "Hello World!" on the homepage
-	app.get('/', function (req, res) {
-			 collection.find().toArray(function(err, results) { // callback arguments are err or an array of results
-
-
-				for(var i = 0; i < results.length; i++) {
-				   console.log(results[i]);
-				}	});
-	});
-
-	// accept POST request on the homepage
-	app.post('/', function (req, res) {
-	  res.send('Got a POST request');
-	});
-
-	// accept PUT request at /user
-	app.put('/user/:username', function (req, res) {
-	  var username = req.params.username;
-	  res.send('Hello, '+username);
-	});
-
-	// accept DELETE request at /user
-	app.delete('/user', function (req, res) {
-	  res.send('Got a DELETE request at /user');
-	});
 	
      /* Now that we are connected,
         we can run our find command just
@@ -62,6 +36,30 @@ mongoClient.connect("mongodb://localhost:27017/store", function(err, db) {
   } // end if !err
 }); // end mongo connect callback
 
+// respond with "Hello World!" on the homepage
+app.get('/', function (req, res) {
+	if(collection){
+		collection.find().toArray(function(err, results) { // callback arguments are err or an array of results
+			res.send(results);
+		});
+	}
+});
+
+// accept POST request on the homepage
+app.post('/', function (req, res) {
+	res.send('Got a POST request');
+});
+
+// accept PUT request at /user
+app.put('/user/:username', function (req, res) {
+	var username = req.params.username;
+	res.send('Hello, '+username);
+});
+
+// accept DELETE request at /user
+app.delete('/user', function (req, res) {
+	res.send('Got a DELETE request at /user');
+});
 
 var server = app.listen(8024, function () {
 
